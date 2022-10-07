d9342fb3780e89cb571b6c843a8d29d73cecdd30
rohitajay <90746660+rohitajay@users.noreply.github.com>
2022-10-07 18:41:19 +0530

    Update metadata.java

--- metadata.java
+++ metadata.java
@@ -1,4 +1,7 @@
+package testdb;
+
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,28 +18,31 @@ public class metaData {
             conn = DriverManager.getConnection(CONNECTION_STRING, "postgres", "9419131674");
             Statement statement = conn.createStatement();
             DatabaseMetaData databaseMetaData = conn.getMetaData();
-            String query = "SELECT *\n" +
+            String query = "SELECT table_catalog, table_schema, table_name, column_name, ordinal_position, column_default, is_nullable, data_type, character_maximum_length,numeric_precision, udt_catalog \n" +
                     "  FROM information_schema.columns\n" +
-                    " WHERE table_schema = 'public';";
+                    " WHERE table_schema = 'public' LIMIT 5;";
             ResultSet resultSet = statement.executeQuery(query);
             ResultSetMetaData rsmd = resultSet.getMetaData();
             int columnCount = rsmd.getColumnCount();
+            System.out.println(columnCount);
             List<String> column_list = new ArrayList<String>();
             for (int i = 1; i < columnCount; i++) {
                 column_list.add(rsmd.getColumnLabel(i));
             }
             System.out.println(column_list);
             List<List<String>> value_list = new ArrayList<>();
+            List<String> row_string = new ArrayList<>();
+
             while (resultSet.next()) {
-                List<String> row_string = new ArrayList<String>();
-
-                for (int j = 0; j < columnCount - 1; j++) {
-
-                    row_string.add(resultSet.getString(column_list.get(j)));
-                    value_list.add(row_string);
+                List<String> single_row = new ArrayList<>();
+                for (String element: column_list){
+                    single_row.add(resultSet.getString(element));
+                    }
+//
+                value_list.add(single_row);
                 }
             System.out.println(value_list);
-            }
+
         } catch (SQLException e) {
             System.out.println("Something went wrong " + e.getMessage());
         }
